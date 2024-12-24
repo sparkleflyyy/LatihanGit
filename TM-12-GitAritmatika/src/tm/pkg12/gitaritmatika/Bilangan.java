3a3fafa26f1927af20e9fef0ca775b08ac8b2631
Riyann <riyannurizqy@gmail.com>
2024-12-24 14:47:07 +0700

    Initialization Project

--- TM-12-GitAritmatika/src/tm/pkg12/gitaritmatika/Bilangan.java
+++ TM-12-GitAritmatika/src/tm/pkg12/gitaritmatika/Bilangan.java
@@ -9,5 +9,32 @@ package tm.pkg12.gitaritmatika;
  * @author riyann
  */
 public class Bilangan {
+    private double bilangan1;
+    private double bilangan2;
+
+    public Bilangan() {
+    }
+
+    public Bilangan(double bilangan1, double bilangan2) {
+        this.bilangan1 = bilangan1;
+        this.bilangan2 = bilangan2;
+    }
+
+    public double getBilangan1() {
+        return bilangan1;
+    }
+
+    public void setBilangan1(double bilangan1) {
+        this.bilangan1 = bilangan1;
+    }
+
+    public double getBilangan2() {
+        return bilangan2;
+    }
+
+    public void setBilangan2(double bilangan2) {
+        this.bilangan2 = bilangan2;
+    }
+    
     
 }
